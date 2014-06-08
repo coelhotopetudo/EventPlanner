0eb3ab657e8365e5d82816c0488386029f066ad0
coelhotopetudo <coelhotopetudo@gmail.com>
2014-06-08 16:41:24 -0300

    Mais anotações.

--- doc/Evento.mm
+++ doc/Evento.mm
@@ -17,7 +17,12 @@
 <node CREATED="1402253077520" ID="ID_1420936424" MODIFIED="1402253144656" TEXT="mas como ficar&#xe1; nos pr&#xf3;ximos dias em que ele j&#xe1; pegou o crach&#xe1;?"/>
 </node>
 <node CREATED="1402253241088" ID="ID_1214663715" MODIFIED="1402253256855" POSITION="left" TEXT="quais os dados necess&#xe1;rios estar no QR code">
-<node CREATED="1402253292627" ID="ID_1113815923" MODIFIED="1402253302678" TEXT="para n&#xe3;o interferir na leitura"/>
+<node CREATED="1402253292627" ID="ID_1113815923" MODIFIED="1402256129481" TEXT="para n&#xe3;o interferir na leitura, pois muitos dados podem demorar e at&#xe9; atrapalhar"/>
+<node CREATED="1402255943609" ID="ID_1895046091" MODIFIED="1402255982089" TEXT="cod de inscricao com digito verificador">
+<node CREATED="1402255987196" ID="ID_1554359208" MODIFIED="1402256013739" TEXT="9999-36"/>
+<node CREATED="1402256016841" ID="ID_495546833" MODIFIED="1402256031639" TEXT="99999-45"/>
+</node>
+<node CREATED="1402256079821" ID="ID_1959431726" MODIFIED="1402256084461" TEXT="primeiro nome"/>
 </node>
 <node CREATED="1402253304069" ID="ID_623327025" MODIFIED="1402253335217" POSITION="right" TEXT="e se o QR code molhar ou for danificado?">
 <node CREATED="1402253336618" ID="ID_977883919" MODIFIED="1402253364838" TEXT="o sistema deve permitir a entrada via c&#xf3;digo de inscri&#xe7;&#xe3;o"/>
@@ -25,5 +30,17 @@
 <node CREATED="1402253367403" ID="ID_882982955" MODIFIED="1402253380014" POSITION="left" TEXT="o c&#xf3;digo de inscri&#xe7;&#xe3;o deve ficar vis&#xed;vel no crach&#xe1;"/>
 <node CREATED="1402253380248" ID="ID_1457900094" MODIFIED="1402253409518" POSITION="left" TEXT="o c&#xf3;digo de inscri&#xe7;&#xe3;o deve ser de f&#xe1;cil acesso no site para que o inscrito, possa recuper&#xe1;-lo caso perca o crach&#xe1;"/>
 <node CREATED="1402253447108" ID="ID_429973736" MODIFIED="1402253474611" POSITION="right" TEXT="o app grade nao deveria ser somente para android"/>
+<node CREATED="1402255517216" ID="ID_359690927" MODIFIED="1402255540075" POSITION="right" TEXT="a associa&#xe7;&#xe3;o do crach&#xe1; coringa &#xe9; muito importante, por isso devemos frisar na hora do cadastramento"/>
+<node CREATED="1402255652702" ID="ID_1172432720" MODIFIED="1402255684453" POSITION="right" TEXT="a identificacao do coringa poderia ter um digito verificador">
+<node CREATED="1402255689413" ID="ID_1574072971" MODIFIED="1402255714776" TEXT="devido a importancia dele para assegurar a confirma&#xe7;ao de presenca em minicurso"/>
+</node>
+<node CREATED="1402255728370" ID="ID_1057846224" MODIFIED="1402255761971" POSITION="left" TEXT="validar id do coringa na hora da associa&#xe7;&#xe3;o com o cadastro do usu&#xe1;rio"/>
+<node CREATED="1402255773625" ID="ID_25527466" MODIFIED="1402255792383" POSITION="left" TEXT="teste de stress para cada coletor que confirmar&#xe1; presenca no evento">
+<node CREATED="1402255794185" ID="ID_606283268" MODIFIED="1402255822871" TEXT="algo como ler e armazenar 500 registros de presenca"/>
+</node>
+<node CREATED="1402255848067" ID="ID_1957775013" MODIFIED="1402255863010" POSITION="right" TEXT="digito verificador em todos os qrcodes">
+<node CREATED="1402255863855" ID="ID_576758615" MODIFIED="1402255871122" TEXT="ser&#xe1; que isso nao seria mais uma seguranca"/>
+<node CREATED="1402255872419" ID="ID_459104643" MODIFIED="1402255920999" TEXT="por exemplo o c&#xf3;digo de inscricao &#xe9; 123 e o armazenado do qrcode seria 123-06"/>
+</node>
 </node>
 </map>
