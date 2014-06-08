efc6481b44ed22be07e316166f77c40ddb8f3c9c
coelhotopetudo <coelhotopetudo@gmail.com>
2014-06-08 15:52:33 -0300

    Algumas idéias e fluxos.

---
+++ doc/Evento.mm
@@ -0,0 +1,29 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1402252755439" ID="ID_76723087" MODIFIED="1402252929939" TEXT="Evento">
+<node CREATED="1402252763859" ID="ID_681345471" MODIFIED="1402252781535" POSITION="right" TEXT="coletor">
+<node CREATED="1402253157925" ID="ID_654117600" MODIFIED="1402253167351" TEXT="quantos coletores teremos?"/>
+<node CREATED="1402253176053" ID="ID_1038185596" MODIFIED="1402253204101" TEXT="precisamos de uns 3 para confirma&#xe7;&#xe3;o de presen&#xe7;a ap&#xf3;s crach&#xe1;"/>
+<node CREATED="1402253205873" ID="ID_1169220570" MODIFIED="1402253214694" TEXT="e um para casa laborat&#xf3;rio"/>
+</node>
+<node CREATED="1402252783300" ID="ID_1622161692" MODIFIED="1402252809318" POSITION="right" TEXT="confirmar a presen&#xe7;a ap&#xf3;s entrega do cracha"/>
+<node CREATED="1402252823363" ID="ID_1883907559" MODIFIED="1402252835608" POSITION="left" TEXT="atualizar telas mostrando as vagas disponiveis"/>
+<node CREATED="1402252836016" ID="ID_1023841895" MODIFIED="1402252854566" POSITION="left" TEXT="periodo para o check-in">
+<node CREATED="1402252855753" ID="ID_761844515" MODIFIED="1402252874442" TEXT="para confirma&#xe7;&#xe3;o da presen&#xe7;a no minicurso"/>
+<node CREATED="1402252960063" ID="ID_632413839" MODIFIED="1402252995100" TEXT="de 3 a 1 hora antes inicio do minicurso"/>
+<node CREATED="1402252998518" ID="ID_921149734" MODIFIED="1402253013005" TEXT="temos toda a infra/implementa&#xe7;&#xe3;o para fazer esse controle?"/>
+</node>
+<node CREATED="1402253047898" ID="ID_1950740444" MODIFIED="1402253076847" POSITION="right" TEXT="confirma&#xe7;&#xe3;o de presen&#xe7;a ap&#xf3;s entrega do crach&#xe1; tamb&#xe9;m servir para check-in minicurso">
+<node CREATED="1402253077520" ID="ID_1420936424" MODIFIED="1402253144656" TEXT="mas como ficar&#xe1; nos pr&#xf3;ximos dias em que ele j&#xe1; pegou o crach&#xe1;?"/>
+</node>
+<node CREATED="1402253241088" ID="ID_1214663715" MODIFIED="1402253256855" POSITION="left" TEXT="quais os dados necess&#xe1;rios estar no QR code">
+<node CREATED="1402253292627" ID="ID_1113815923" MODIFIED="1402253302678" TEXT="para n&#xe3;o interferir na leitura"/>
+</node>
+<node CREATED="1402253304069" ID="ID_623327025" MODIFIED="1402253335217" POSITION="right" TEXT="e se o QR code molhar ou for danificado?">
+<node CREATED="1402253336618" ID="ID_977883919" MODIFIED="1402253364838" TEXT="o sistema deve permitir a entrada via c&#xf3;digo de inscri&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1402253367403" ID="ID_882982955" MODIFIED="1402253380014" POSITION="left" TEXT="o c&#xf3;digo de inscri&#xe7;&#xe3;o deve ficar vis&#xed;vel no crach&#xe1;"/>
+<node CREATED="1402253380248" ID="ID_1457900094" MODIFIED="1402253409518" POSITION="left" TEXT="o c&#xf3;digo de inscri&#xe7;&#xe3;o deve ser de f&#xe1;cil acesso no site para que o inscrito, possa recuper&#xe1;-lo caso perca o crach&#xe1;"/>
+<node CREATED="1402253447108" ID="ID_429973736" MODIFIED="1402253474611" POSITION="right" TEXT="o app grade nao deveria ser somente para android"/>
+</node>
+</map>
