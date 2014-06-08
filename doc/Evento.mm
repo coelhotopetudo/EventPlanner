9fcc335a2dea0c86dfe38cd2831a70b63ab3b800
coelhotopetudo <coelhotopetudo@gmail.com>
2014-06-08 19:31:35 -0300

    Organização e mais glossário.

--- doc/Evento.mm
+++ doc/Evento.mm
@@ -1,19 +1,27 @@
 <map version="0.9.0">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1402252755439" ID="ID_76723087" MODIFIED="1402260657312" STYLE="bubble" TEXT="Evento">
-<node CREATED="1402252763859" FOLDED="true" ID="ID_681345471" MODIFIED="1402260657309" POSITION="right" STYLE="bubble" TEXT="qtde celulares (credencto/coletor)">
+<node CREATED="1402252763859" ID="ID_681345471" MODIFIED="1402266377929" POSITION="right" STYLE="bubble" TEXT="qtde celulares (credencto/coletor)">
 <node CREATED="1402253157925" ID="ID_654117600" MODIFIED="1402260334799" STYLE="bubble" TEXT="quantos celular para facilitadores de credenciamento e coletores teremos?"/>
 <node CREATED="1402253176053" ID="ID_1038185596" MODIFIED="1402260334799" STYLE="bubble" TEXT="precisamos de uns 3 para confirma&#xe7;&#xe3;o de presen&#xe7;a ap&#xf3;s crach&#xe1;"/>
 <node CREATED="1402253205873" ID="ID_1169220570" MODIFIED="1402260334799" STYLE="bubble" TEXT="e um para casa laborat&#xf3;rio"/>
 </node>
-<node CREATED="1402252783300" ID="ID_1622161692" MODIFIED="1402260334799" POSITION="right" STYLE="bubble" TEXT="confirmar a presen&#xe7;a ap&#xf3;s entrega do cracha"/>
-<node CREATED="1402260251750" FOLDED="true" ID="ID_1767446024" MODIFIED="1402260657310" POSITION="left" STYLE="bubble" TEXT="Gloss&#xe1;rio">
+<node CREATED="1402252783300" ID="ID_1622161692" MODIFIED="1402266158037" POSITION="right" STYLE="bubble" TEXT="confirmar a presen&#xe7;a antes da entrega do cracha">
+<node CREATED="1402266140597" ID="ID_239757413" MODIFIED="1402266145656" TEXT="lendo o qrcode"/>
+</node>
+<node CREATED="1402260251750" ID="ID_1767446024" MODIFIED="1402266394563" POSITION="left" STYLE="bubble" TEXT="Gloss&#xe1;rio">
 <node CREATED="1402260293488" FOLDED="true" ID="ID_1549210247" MODIFIED="1402260657309" STYLE="bubble" TEXT="Coletor">
 <node CREATED="1402260297816" ID="ID_16823736" MODIFIED="1402260334799" STYLE="bubble" TEXT="Pessoa que estar&#xe1; nos laborat&#xf3;rios verificando os inscritos no minicurso"/>
 </node>
 <node CREATED="1402260255060" FOLDED="true" ID="ID_782770971" MODIFIED="1402260657310" STYLE="bubble" TEXT="Credenciador">
 <node CREATED="1402260260957" ID="ID_1547729337" MODIFIED="1402260334799" STYLE="bubble" TEXT="Ou facilitador de credenciamento"/>
 <node CREATED="1402260268572" ID="ID_1747713516" MODIFIED="1402260334799" STYLE="bubble" TEXT="Pessoa respons&#xe1;vel por dar o crach&#xe1; e confirmar a presen&#xe7;a no evento"/>
+</node>
+<node CREATED="1402266396300" ID="ID_1024380027" MODIFIED="1402266412103" TEXT="P&#xf3;s-inscritos (ou p&#xf3;s)">
+<node CREATED="1402266413050" ID="ID_590199436" MODIFIED="1402266432939" TEXT="Pessoas que fizeram a inscri&#xe7;&#xe3;o para o evento no dia do evento"/>
+</node>
+<node CREATED="1402266435755" ID="ID_950592806" MODIFIED="1402266444879" TEXT="Pr&#xe9;-inscritos (ou pr&#xe9;)">
+<node CREATED="1402266446421" ID="ID_1165354638" MODIFIED="1402266459684" TEXT="Pessoas que fizeram a inscri&#xe7;&#xe3;o previamente"/>
 </node>
 </node>
 <node CREATED="1402252823363" ID="ID_1883907559" MODIFIED="1402260334800" POSITION="left" STYLE="bubble" TEXT="atualizar telas mostrando as vagas disponiveis"/>
@@ -25,31 +33,39 @@
 <node CREATED="1402253047898" FOLDED="true" ID="ID_1950740444" MODIFIED="1402260941183" POSITION="right" STYLE="bubble" TEXT="confirma&#xe7;&#xe3;o de presen&#xe7;a ap&#xf3;s entrega do crach&#xe1; tamb&#xe9;m servir para check-in minicurso">
 <node CREATED="1402253077520" ID="ID_1420936424" MODIFIED="1402260334800" STYLE="bubble" TEXT="mas como ficar&#xe1; nos pr&#xf3;ximos dias em que ele j&#xe1; pegou o crach&#xe1;?"/>
 </node>
-<node CREATED="1402253241088" FOLDED="true" ID="ID_1214663715" MODIFIED="1402260657311" POSITION="left" STYLE="bubble" TEXT="quais os dados necess&#xe1;rios estar no QR code">
-<node CREATED="1402253292627" ID="ID_1113815923" MODIFIED="1402260334800" STYLE="bubble" TEXT="para n&#xe3;o interferir na leitura, pois muitos dados podem demorar e at&#xe9; atrapalhar"/>
-<node CREATED="1402255943609" FOLDED="true" ID="ID_1895046091" MODIFIED="1402260657310" STYLE="bubble" TEXT="cod de inscricao com digito verificador">
-<node CREATED="1402255987196" ID="ID_1554359208" MODIFIED="1402260334800" STYLE="bubble" TEXT="9999-36"/>
-<node CREATED="1402256016841" ID="ID_495546833" MODIFIED="1402260334800" STYLE="bubble" TEXT="99999-45"/>
+<node CREATED="1402253241088" FOLDED="true" ID="ID_1214663715" MODIFIED="1402266330796" POSITION="left" STYLE="bubble" TEXT="quais os dados necess&#xe1;rios estar no QR code">
+<node CREATED="1402253292627" ID="ID_1113815923" MODIFIED="1402260334800" STYLE="bubble" TEXT="para n&#xe3;o interferir na leitura, pois muitos dados podem demorar e at&#xe9; atrapalhar">
+<node CREATED="1402266299309" ID="ID_18301915" MODIFIED="1402266307120" TEXT="ser&#xe1; q o e-mail &#xe9; necess&#xe1;rio?"/>
 </node>
-<node CREATED="1402256079821" ID="ID_1959431726" MODIFIED="1402260334800" STYLE="bubble" TEXT="primeiro nome"/>
+<node CREATED="1402255943609" ID="ID_1895046091" MODIFIED="1402266284061" STYLE="bubble" TEXT="cod de inscricao com digito verificador">
+<arrowlink DESTINATION="ID_1957775013" ENDARROW="Default" ENDINCLINATION="843;0;" ID="Arrow_ID_1560165167" STARTARROW="None" STARTINCLINATION="843;0;"/>
 </node>
-<node CREATED="1402253304069" FOLDED="true" ID="ID_623327025" MODIFIED="1402260657311" POSITION="right" STYLE="bubble" TEXT="e se o QR code molhar ou for danificado?">
+<node CREATED="1402256079821" ID="ID_1959431726" MODIFIED="1402260334800" STYLE="bubble" TEXT="primeiro nome">
+<node CREATED="1402266316274" ID="ID_729312282" MODIFIED="1402266322766" TEXT="ao inv&#xe9;s do nome completo"/>
+</node>
+</node>
+<node CREATED="1402253304069" FOLDED="true" ID="ID_623327025" MODIFIED="1402266367951" POSITION="right" STYLE="bubble" TEXT="e se o QR code molhar ou for danificado?">
 <node CREATED="1402253336618" ID="ID_977883919" MODIFIED="1402260334800" STYLE="bubble" TEXT="o sistema deve permitir a entrada via c&#xf3;digo de inscri&#xe7;&#xe3;o"/>
+<node CREATED="1402266175379" ID="ID_554540124" MODIFIED="1402266222825" TEXT="o digito verificador garantir&#xe1; a leitura correta do c&#xf3;digo">
+<arrowlink DESTINATION="ID_1957775013" ENDARROW="Default" ENDINCLINATION="363;0;" ID="Arrow_ID_1476763860" STARTARROW="None" STARTINCLINATION="363;0;"/>
+</node>
 </node>
 <node CREATED="1402253367403" ID="ID_882982955" MODIFIED="1402260334800" POSITION="left" STYLE="bubble" TEXT="o c&#xf3;digo de inscri&#xe7;&#xe3;o deve ficar vis&#xed;vel no crach&#xe1;"/>
 <node CREATED="1402253380248" ID="ID_1457900094" MODIFIED="1402260334800" POSITION="left" STYLE="bubble" TEXT="o c&#xf3;digo de inscri&#xe7;&#xe3;o deve ser de f&#xe1;cil acesso no site para que o inscrito, possa recuper&#xe1;-lo caso perca o crach&#xe1;"/>
-<node CREATED="1402253447108" ID="ID_429973736" MODIFIED="1402260334803" POSITION="right" STYLE="bubble" TEXT="o app grade nao deveria ser somente para android"/>
 <node CREATED="1402255517216" ID="ID_359690927" MODIFIED="1402260334803" POSITION="right" STYLE="bubble" TEXT="a associa&#xe7;&#xe3;o do crach&#xe1; coringa &#xe9; muito importante, por isso devemos frisar na hora do cadastramento"/>
 <node CREATED="1402255652702" FOLDED="true" ID="ID_1172432720" MODIFIED="1402260657311" POSITION="right" STYLE="bubble" TEXT="a identificacao do coringa poderia ter um digito verificador">
 <node CREATED="1402255689413" ID="ID_1574072971" MODIFIED="1402260334803" STYLE="bubble" TEXT="devido a importancia dele para assegurar a confirma&#xe7;ao de presenca em minicurso"/>
 </node>
 <node CREATED="1402255728370" ID="ID_1057846224" MODIFIED="1402260334803" POSITION="left" STYLE="bubble" TEXT="validar id do coringa na hora da associa&#xe7;&#xe3;o com o cadastro do usu&#xe1;rio"/>
-<node CREATED="1402255773625" FOLDED="true" ID="ID_25527466" MODIFIED="1402260657311" POSITION="left" STYLE="bubble" TEXT="teste de stress para cada coletor que confirmar&#xe1; presenca no evento">
+<node CREATED="1402255773625" ID="ID_25527466" MODIFIED="1402266256839" POSITION="left" STYLE="bubble" TEXT="teste de stress para cada coletor que confirmar&#xe1; presenca no evento">
 <node CREATED="1402255794185" ID="ID_606283268" MODIFIED="1402260334803" STYLE="bubble" TEXT="algo como ler e armazenar 500 registros de presenca"/>
 </node>
-<node CREATED="1402255848067" FOLDED="true" ID="ID_1957775013" MODIFIED="1402260657311" POSITION="right" STYLE="bubble" TEXT="digito verificador em todos os qrcodes">
-<node CREATED="1402255863855" ID="ID_576758615" MODIFIED="1402260334803" STYLE="bubble" TEXT="ser&#xe1; que isso nao seria mais uma seguranca"/>
-<node CREATED="1402255872419" ID="ID_459104643" MODIFIED="1402260334803" STYLE="bubble" TEXT="por exemplo o c&#xf3;digo de inscricao &#xe9; 123 e o armazenado do qrcode seria 123-06"/>
+<node CREATED="1402255848067" FOLDED="true" ID="ID_1957775013" MODIFIED="1402266366525" POSITION="right" STYLE="bubble" TEXT="digito verificador em todos os qrcodes">
+<node CREATED="1402255863855" ID="ID_576758615" MODIFIED="1402266249506" STYLE="bubble" TEXT="par dar mais uma seguranca"/>
+<node CREATED="1402255872419" ID="ID_459104643" MODIFIED="1402260334803" STYLE="bubble" TEXT="por exemplo o c&#xf3;digo de inscricao &#xe9; 123 e o armazenado do qrcode seria 123-06">
+<node CREATED="1402255987196" ID="ID_1554359208" MODIFIED="1402260334800" STYLE="bubble" TEXT="9999-36"/>
+<node CREATED="1402256016841" ID="ID_495546833" MODIFIED="1402260334800" STYLE="bubble" TEXT="99999-45"/>
+</node>
 </node>
 <node CREATED="1402260018123" FOLDED="true" ID="ID_861052215" MODIFIED="1402260657311" POSITION="left" STYLE="bubble" TEXT="imprimir lista de inscritos evento">
 <node CREATED="1402260026806" ID="ID_262554094" MODIFIED="1402260334803" STYLE="bubble" TEXT="para se der algum problema com os celulares"/>
@@ -75,12 +91,20 @@
 <node CREATED="1402261011326" ID="ID_1835028405" MODIFIED="1402261024424" TEXT="algo parecido com o check-in de voo"/>
 </node>
 <node CREATED="1402265536886" ID="ID_435442607" MODIFIED="1402265555025" POSITION="left" TEXT="ser&#xe1; q 100 qr coringas seriam suficientes">
-<node CREATED="1402265555995" ID="ID_506561179" MODIFIED="1402265579756" TEXT="ser&#xe1; que precisamos de qr para todos os nao inscritos?">
+<node CREATED="1402265555995" ID="ID_506561179" MODIFIED="1402266392728" TEXT="ser&#xe1; que precisamos de qr para todos os pos inscritos?">
 <node CREATED="1402265580574" ID="ID_634283249" MODIFIED="1402265645753" TEXT="avisar o pos de que se quiser participar de minicurso precisa do QR code ou c&#xf3;digo de inscricao"/>
 </node>
 </node>
 <node CREATED="1402265673069" ID="ID_321921711" MODIFIED="1402265706055" POSITION="right" TEXT="precisamos realmente do coringa ou poderiamos fazer o controle manual?"/>
 <node CREATED="1402265751165" ID="ID_1799020986" MODIFIED="1402265809461" POSITION="left" TEXT="poder&#xed;amos pedir para manter os cr&#xe9;ditos de onde foi gerado quando alguma outra organizacao for distribuir, por exemplo no google play"/>
 <node CREATED="1402265882148" ID="ID_1981340840" MODIFIED="1402265905305" POSITION="right" TEXT="quem ser&#xe1; o mestre de cerim&#xf4;nias da noite?"/>
+<node CREATED="1402266612871" FOLDED="true" ID="ID_930381248" MODIFIED="1402266651917" POSITION="left" TEXT="Repassado">
+<node CREATED="1402266623096" ID="ID_360293539" MODIFIED="1402266649280" TEXT="Repassado para outro lugar ou forma de organiza&#xe7;&#xe3;o">
+<cloud/>
+</node>
+<node CREATED="1402253447108" ID="ID_429973736" MODIFIED="1402266504040" STYLE="bubble" TEXT="o app grade nao deve ser somente para android">
+<node CREATED="1402266643187" ID="ID_190398876" MODIFIED="1402266647216" TEXT="trello"/>
+</node>
+</node>
 </node>
 </map>
